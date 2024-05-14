f3afc9272ecd0c83739ad2a3f7fcbd4e41858ae2
You-NaSong <twins7534@naver.com>
2024-05-14 18:21:12 +0900

    로그인/회원가입 구현

---
+++ login.js
@@ -0,0 +1,27 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+const signUpForm = document.querySelector('.sign-up-container');
+const signInForm = document.querySelector('.sign-in-container');
+const signUpBtn = document.getElementById('signUpButton');
+const signInBtn = document.getElementById('signInButton');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+  container.classList.remove("right-panel-active");
+});
+
+signInBtn.addEventListener('click', (event) => {
+     event.preventDefault();
+     alert('로그인이 완료되었습니다.');
+    // 여기에 로그인을 처리하는 코드를 추가할 수 있습니다.
+ });
+
+signUpBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    alert('회원가입이 완료되었습니다.');
+    // 여기에 회원가입을 처리하는 코드를 추가할 수 있습니다.
+});
