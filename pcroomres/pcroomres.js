b51d64c033a4dc03619bcdb07571584d24bf57e7
You-NaSong <twins7534@naver.com>
2024-05-23 20:01:29 +0900

    예약하기
    좌석 선택 후 예약하기 버튼 누르면 선택좌석 다시 0으로 변환, 이미예약된 좌석으로 변경

--- pcroomres/pcroomres.js
+++ pcroomres/pcroomres.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const seatContainer = document.querySelector('.seatContainer');
+    const resButton = document.getElementById('res');
+    const count = document.getElementById('count');
 
     const pcroom = document.getElementById('pcroom'); // 선택할 피시방
-    let pcroomres = Number(pcroom.value); // 피시방 가격 
-
-    let count = document.querySelector('#count'); // 인원수
 
     // 선택한 좌석수 텍스트 변경해주기
     function countSeatlist() {
@@ -25,11 +24,31 @@ document.addEventListener('DOMContentLoaded', () => {
         countSeatlist();
     });
 
+
+    // 선택된 좌석 개수를 0으로 초기화하는 함수
+    function resetSelectedSeatCount() {
+        count.textContent = '0';
+    }
+
+     function markSeatsAsOccupied() {
+        const selectedSeats = document.querySelectorAll('.selectedSeat');
+
+        selectedSeats.forEach(seat => {
+            seat.classList.remove('selectedSeat');
+            seat.classList.add('occupiedSeat');
+            seat.textContent = ''; // 선택된 좌석의 텍스트 내용을 제거합니다.
+        });
+    }
+
     // 예약하기 버튼 클릭했을때
     const reserveButton = document.getElementById('res');
     reserveButton.addEventListener('click', () => {
         alert('예약이 완료되었습니다.');
-    });
 
-    // 피시방 변경할때는 가격을 업데이트할 필요가 없으므로 해당 부분은 삭제
+        // 예약이 완료된 후에 occupiedSeat으로 변경하는 함수를 호출합니다.
+        markSeatsAsOccupied();
+
+        // 선택된 좌석 개수를 0으로 초기화합니다.
+        resetSelectedSeatCount();
+    });
 });
