1764cb27b5e4d65d177256937cf051f702ae1661
You-NaSong <twins7534@naver.com>
2024-05-29 18:01:11 +0900

    modal창 구현
    피시방리스트, 모달창이 지도보다 뒤로 뜨는 이슈

---
+++ pcroomlist/modal.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector('.modal');
+const modalOpen = document.querySelector('.modal_btn');
+const modalClose = document.querySelector('.close_btn');
+
+//열기 버튼을 눌렀을 때 모달팝업이 열림
+modalOpen.addEventListener('click',function(){
+  	//'on' class 추가
+    modal.classList.add('on');
+});
+//닫기 버튼을 눌렀을 때 모달팝업이 닫힘
+modalClose.addEventListener('click',function(){
+    //'on' class 제거
+    modal.classList.remove('on');
+});
--- pcroomres/pcroomres.js
+++ pcroomres/pcroomres.js
@@ -50,3 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+
+
